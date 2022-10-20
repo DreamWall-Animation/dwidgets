9b07bf0d9fea80ef054b313a8c9b070ef82fc722
Lionel Brouyere <lionel.brouyere@gmail.com>
2022-10-20 14:42:59 +0200

    Change init shortcuts

--- dwidgets/__init__.py
+++ dwidgets/__init__.py
@@ -1,2 +1,5 @@
-from dwidgets.weightslider import WeightSlider
+
+from dwidgets.calendar import CalendarDialog
+from dwidgets.tag import TagView
 from dwidgets.tilteddates import TiltedDates
+from dwidgets.weightslider import WeightSlider
