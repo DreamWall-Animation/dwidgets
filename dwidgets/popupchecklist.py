5ce20277a3d9b105f0cc563b2f62b53bd424e032
ndeebook <evers@dreamwall.be>
2022-11-14 14:49:17 +0100

    change PopupCheckList

--- dwidgets/popupchecklist.py
+++ dwidgets/popupchecklist.py
@@ -3,13 +3,14 @@ from PySide2 import QtWidgets, QtCore
 from PySide2.QtCore import Qt
 
 
-class PopupCheckList(QtWidgets.QPushButton):
+class PopupCheckList(QtWidgets.QWidget):
     checked_items_changed = QtCore.Signal(list)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.checkboxes = []
+        self.setWindowFlags(Qt.Popup | QtCore.Qt.FramelessWindowHint)
 
         self.list_widget = QtWidgets.QListWidget()
         self.list_widget.itemClicked.connect(self._toggle_checkbox)
@@ -23,11 +24,10 @@ class PopupCheckList(QtWidgets.QPushButton):
             'all', clicked=self.check_all,
             maximumHeight=30, minimumWidth=60)
 
-        self.menu = QtWidgets.QMenu()
         menu_layout = QtWidgets.QVBoxLayout()
         menu_layout.setContentsMargins(0, 0, 0, 0)
         menu_layout.setSpacing(0)
-        self.menu.setLayout(menu_layout)
+        self.setLayout(menu_layout)
         menu_layout.addWidget(self.list_widget)
         buttons_layout = QtWidgets.QHBoxLayout()
         menu_layout.addLayout(buttons_layout)
@@ -35,36 +35,15 @@ class PopupCheckList(QtWidgets.QPushButton):
         buttons_layout.addWidget(invert_btn)
         buttons_layout.addWidget(all_btn)
 
-        self.clicked.connect(self.popup)
-
-    def mousePressEvent(self, event):
-        if event.type() == QtCore.QEvent.MouseButtonPress:
-            if event.button() == Qt.MiddleButton:
-                self.uncheck_all()
-            elif event.button() == Qt.RightButton:
-                self.invert()
-        return super().mousePressEvent(event)
-
     def _toggle_checkbox(self, item):
         cb = self.list_widget.itemWidget(item)
         cb.setChecked(not cb.isChecked())
-
-    def _set_text(self):
-        labels = self.checked_items_labels()
-        if not labels or len(labels) == len(self.checkboxes):
-            text = ' '
-        elif len(labels) == 1:
-            text = labels[0]
-        else:
-            text = f'({len(labels)}/{len(self.checkboxes)})'
-        self.setText(text)
 
     def set_items(self, labels):
         self.checkboxes.clear()
         self.list_widget.clear()
         for label in labels:
             cb = QtWidgets.QCheckBox(label)
-            cb.stateChanged.connect(self._set_text)
             cb.stateChanged.connect(self._send_signal)
             host_item = QtWidgets.QListWidgetItem()
             self.list_widget.addItem(host_item)
@@ -96,14 +75,44 @@ class PopupCheckList(QtWidgets.QPushButton):
         with self._single_signal():
             [cb.setChecked(not cb.isChecked()) for cb in self.checkboxes]
 
-    def popup(self):
+    def popup(self, position):
         if not self.checkboxes:
             return
-        parent = self.parent()
-        self.list_widget.setMinimumWidth(self.width())
-        checkbox_height = self.checkboxes[0].height() + 2
-        pos = self.pos()
-        self.list_widget.setMinimumHeight(min(
-            self.list_widget.count() * checkbox_height, parent.height() / 3))
-        pos.setY(pos.y() + self.height())
-        self.menu.popup(parent.mapToGlobal(pos))
+        self.move(position)
+        self.show()
+
+
+class PopupCheckListButton(QtWidgets.QPushButton):
+    checked_items_changed = QtCore.Signal(list)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.menu = PopupCheckList(self)
+        self.clicked.connect(self.popup)
+        self.menu.checked_items_changed.connect(self._set_text)
+
+        self.set_items = self.menu.set_items
+        self.checked_items_labels = self.menu.checked_items_labels
+
+    def popup(self):
+        position = self.mapToGlobal(self.rect().bottomLeft())
+        self.menu.popup(position)
+
+    def _set_text(self):
+        labels = self.menu.checked_items_labels()
+        if not labels or len(labels) == len(self.menu.checkboxes):
+            text = ' '
+        elif len(labels) == 1:
+            text = labels[0]
+        else:
+            text = f'({len(labels)}/{len(self.menu.checkboxes)})'
+        self.setText(text)
+
+    def mousePressEvent(self, event):
+        if event.type() == QtCore.QEvent.MouseButtonPress:
+            if event.button() == Qt.MiddleButton:
+                self.menu.uncheck_all()
+            elif event.button() == Qt.RightButton:
+                self.menu.invert()
+        return super().mousePressEvent(event)
