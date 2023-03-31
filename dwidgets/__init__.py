be72517a387e51e04187a886bc53b57e5a182bb0
Lionel Brouyere <lionel.brouyere@gmail.com>
2023-03-31 15:51:42 +0200

    Add dropfile area

--- dwidgets/__init__.py
+++ dwidgets/__init__.py
@@ -1,6 +1,7 @@
 
 from dwidgets.calendar import CalendarDialog
 from dwidgets.choicemenu import ChoiceMenu, ChoiceScrollMenu
+from dwidgets.dropfilearea import DropFileArea
 from dwidgets.popupchecklist import PopupCheckList
 from dwidgets.retakecanvas.retakecanvas import RetakeCanvas
 from dwidgets.retakecanvas.model import RetakeCanvasModel
---
+++ dwidgets/dropfilearea.py
@@ -0,0 +1,210 @@
+
+from PySide2 import QtWidgets, QtGui, QtCore
+
+
+DEFAULT_BACKGROUND_COLOR = QtCore.Qt.transparent
+DEFAULT_BORDER_COLOR = QtCore.Qt.white
+DEFAULT_TEXT_COLOR = QtCore.Qt.black
+DEFAULT_CROSS_BACKGROUND_COLOR = QtCore.Qt.black
+DEFAULT_CROSS_TEXT_COLOR = QtCore.Qt.white
+DEFAULT_ITEM_SIZE = 60, 60
+DEFAULT_ITEM_SPACING = 13
+DELETER_RADIUS = 15
+TEXT_HEIGHT_SQUARE = 15
+MARGINS = 10, 10, 10, 10
+
+
+def _coordinate_builder(size, spacing, width):
+    left = MARGINS[0]
+    top = MARGINS[1]
+    yield left, top
+    while True:
+        left += spacing + size[0]
+        limit = left + size[0] + spacing + MARGINS[-1]
+        if limit > width:
+            left = MARGINS[0]
+            top += size[1] + spacing
+        yield left, top
+
+
+class DropFileArea(QtWidgets.QWidget):
+    def __init__(
+            self,
+            background_color=None,
+            border_color=None,
+            text_color=None,
+            item_size=None,
+            item_spacing=None,
+            cross_background_color=None,
+            cross_text_color=None,
+            supported_extensions=None,
+            parent=None):
+        super().__init__(parent)
+        self.setMouseTracking(True)
+        supported_extensions = supported_extensions or []
+        self.supported_extensions = [e.lower() for e in supported_extensions]
+        color = cross_background_color or DEFAULT_CROSS_BACKGROUND_COLOR
+        self.cross_background_color = color
+        self.background_color = background_color or DEFAULT_BACKGROUND_COLOR
+        self.cross_text_color = cross_text_color or DEFAULT_CROSS_TEXT_COLOR
+        self.border_color = border_color or DEFAULT_BORDER_COLOR
+        self.text_color = text_color or DEFAULT_TEXT_COLOR
+        self.item_size = item_size or DEFAULT_ITEM_SIZE
+        self.item_spacing = item_spacing or DEFAULT_ITEM_SPACING
+        self.filenames = []
+        self.setAcceptDrops(True)
+        self.hovered_index = None
+        self.setMinimumSize(
+            MARGINS[0] + MARGINS[2] + self.item_size[0] + self.item_spacing,
+            MARGINS[1] + MARGINS[3] + self.item_size[1])
+
+    def sizeHint(self):
+        return QtCore.QSize(
+            MARGINS[0] + MARGINS[2] +
+            (3 * (self.item_size[0] + self.item_spacing)),
+            MARGINS[1] + MARGINS[3] + self.item_size[1])
+
+    def mouseMoveEvent(self, event):
+        coordinates = _coordinate_builder(
+            self.item_size, self.item_spacing, self.width())
+        for i, filename in enumerate(self.filenames):
+            top, left = next(coordinates)
+            rect = QtCore.QRect(top, left, *self.item_size)
+            if rect.contains(event.pos()):
+                self.hovered_index = i
+                self.repaint()
+                self.setToolTip(filename)
+                return
+        self.hovered_index = None
+        self.setToolTip('')
+        self.repaint()
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == QtCore.Qt.LeftButton:
+            if self.hovered_index is None:
+                return
+            del self.filenames[self.hovered_index]
+            self.repaint()
+
+    def dragEnterEvent(self, event):
+        mimedata = event.mimeData()
+        if not mimedata.hasUrls():
+            return False
+        if not self.supported_extensions:
+            return event.accept()
+
+        accept = any(
+            url.path().lower().endswith(tuple(self.supported_extensions))
+            for url in mimedata.urls())
+        if accept:
+            event.accept()
+
+    def dropEvent(self, event):
+        self.filenames.extend([
+            url.toLocalFile() for url in event.mimeData().urls()
+            if not self.supported_extensions or
+            url.toLocalFile().endwith(tuple(self.supported_extensions))])
+        self.repaint()
+
+    def set_colors(
+            self,
+            background_color=None,
+            border_color=None,
+            text_color=None):
+
+        self.background_color = background_color or DEFAULT_BACKGROUND_COLOR
+        self.border_color = border_color or DEFAULT_BORDER_COLOR
+        self.text_color = text_color or DEFAULT_TEXT_COLOR
+        self.repaint()
+
+    def paintEvent(self, _):
+        painter = QtGui.QPainter(self)
+        painter.setBrush(QtGui.QColor(self.background_color))
+        pen = QtGui.QPen(QtGui.QColor(self.border_color))
+        pen.setWidth(5)
+        pen.setStyle(QtCore.Qt.DashLine)
+        painter.setPen(pen)
+        painter.drawRoundedRect(self.rect(), 20, 20)
+        if not self.filenames:
+            painter.setPen(QtGui.QColor(self.text_color))
+            painter.setBrush(QtCore.Qt.NoBrush)
+            font = QtGui.QFont()
+            font.setBold(True)
+            font.setPixelSize(20)
+            painter.setFont(font)
+            text = 'Drop files here'
+            painter.drawText(self.rect(), QtCore.Qt.AlignCenter, text)
+            painter.end()
+            w = MARGINS[0] + MARGINS[2] + self.item_size[0] + self.item_spacing
+            self.setMinimumSize(w, MARGINS[1] + MARGINS[3] + self.item_size[1])
+            return
+
+        coordinates = _coordinate_builder(
+            self.item_size, self.item_spacing, self.width())
+
+        for i, filename in enumerate(self.filenames):
+            left, top = next(coordinates)
+            rect = QtCore.QRectF(left, top, *self.item_size)
+            info = QtCore.QFileInfo(filename)
+            icon = QtWidgets.QFileIconProvider().icon(info)
+            image = icon.pixmap(*self.item_size).toImage()
+            painter.drawImage(icon_rect(rect), image)
+            painter.setPen(QtGui.QColor(self.text_color))
+            painter.setBrush(QtCore.Qt.NoBrush)
+            font = QtGui.QFont()
+            painter.setFont(font)
+            align = QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter
+            painter.drawText(filename_rect(rect), align, filename)
+            pen = QtGui.QPen(QtGui.QColor(self.border_color))
+            pen.setStyle(QtCore.Qt.DotLine)
+            painter.setPen(pen)
+            painter.setBrush(QtCore.Qt.NoBrush)
+            painter.drawRect(filename_rect(rect))
+            if i == self.hovered_index:
+                painter.setPen(QtCore.Qt.NoPen)
+                color = QtGui.QColor(self.cross_background_color)
+                color.setAlpha(125)
+                painter.setBrush(color)
+                drect = deleted_rect(rect)
+                painter.drawEllipse(drect)
+                painter.setPen(QtGui.QColor(self.text_color))
+                painter.setBrush(QtCore.Qt.NoBrush)
+                font = QtGui.QFont()
+                font.setBold(True)
+                font.setPixelSize(20)
+                painter.setFont(font)
+                painter.drawText(drect, QtCore.Qt.AlignCenter, 'X')
+        self.setMinimumHeight(rect.bottom() + MARGINS[3])
+        painter.end()
+
+
+def deleted_rect(rect):
+    center = rect.center()
+    return QtCore.QRect(
+        center.x() - DELETER_RADIUS,
+        center.y() - DELETER_RADIUS,
+        DELETER_RADIUS * 2,
+        DELETER_RADIUS * 2)
+
+
+def icon_rect(rect):
+    return QtCore.QRectF(
+        rect.left() + (TEXT_HEIGHT_SQUARE / 2),
+        rect.top(), rect.width() - TEXT_HEIGHT_SQUARE,
+        rect.height() - TEXT_HEIGHT_SQUARE)
+
+
+def filename_rect(rect):
+    return QtCore.QRect(
+        rect.left(), rect.bottom() - TEXT_HEIGHT_SQUARE,
+        rect.width(), TEXT_HEIGHT_SQUARE)
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication([])
+    w = DropFileArea(
+        border_color='#555555',
+        background_color='#333333',
+        text_color='#CCCCCC')
+    w.show()
+    app.exec_()
