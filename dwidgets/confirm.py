1780ed2fc200158b634be427379cb6754108353c
ndeebook <evers@dreamwall.be>
2023-10-16 15:47:54 +0200

    confirm_dialog: parent arg

--- dwidgets/confirm.py
+++ dwidgets/confirm.py
@@ -1,9 +1,12 @@
 from PySide2 import QtWidgets
 
 
-def confirm_dialog(text, title=''):
+def confirm_dialog(text, title='', parent=None):
     prompt = QtWidgets.QMessageBox(
-        windowTitle=title, text=text, standardButtons=(
+        windowTitle=title,
+        text=text,
+        parent=parent,
+        standardButtons=(
             QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.Abort))
     prompt.setDefaultButton(QtWidgets.QMessageBox.Abort)
     if prompt.exec_() == QtWidgets.QMessageBox.Abort:
