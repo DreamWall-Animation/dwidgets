3d16e357e79f4fff4debe3363a1d40292d624bc7
Lionel Brouyere <lionel.brouyere@gmail.com>
2024-01-02 17:58:21 +0100

    colored tilted dates

--- dwidgets/tilteddates.py
+++ dwidgets/tilteddates.py
@@ -1,4 +1,4 @@
-
+import datetime
 from PySide2 import QtCore, QtWidgets, QtGui
 
 DEFAULT_ROTATION_ANGLE = 90
@@ -26,11 +26,14 @@ class TiltedDates(QtWidgets.QWidget):
         """
         super().__init__(parent)
         self._dates = dates
+        self._current_date = None
         self.display_format = DEFAULT_DISPLAY_DATE_FORMAT
         self.angle = DEFAULT_ROTATION_ANGLE
         self.spacing = DEFAULT_SPACING
         self.font = QtGui.QFont()
         self.color = None
+        self.current_color = None
+        self.todays_color = None
 
     @property
     def dates(self):
@@ -41,6 +44,15 @@ class TiltedDates(QtWidgets.QWidget):
         self._dates = dates
         self.repaint()
 
+    @property
+    def current_date(self):
+        return self._current_date
+
+    @current_date.setter
+    def current_date(self, date):
+        self._current_date = date
+        self.repaint()
+
     def sizeHint(self):
         return QtCore.QSize(600, 600)
 
@@ -49,9 +61,21 @@ class TiltedDates(QtWidgets.QWidget):
         painter.setRenderHint(QtGui.QPainter.Antialiasing)
         painter.setPen(QtCore.Qt.transparent)
         color = self.color or QtWidgets.QApplication.palette().text().color()
-        painter.setBrush(color)
+        today = (
+            self.todays_color or
+            QtWidgets.QApplication.palette().brightText().color())
+        current = (
+            self.current_color or
+            QtWidgets.QApplication.palette().windowText().color())
+
         painter.translate(20, 100)
         for i, d in enumerate(self.dates):
+            if d == self.current_date:
+                painter.setBrush(current)
+            elif d == datetime.date.today():
+                painter.setBrush(today)
+            else:
+                painter.setBrush(color)
             path = QtGui.QPainterPath()
             text = d.strftime(self.display_format)
             point = QtCore.QPoint(0, 0)
