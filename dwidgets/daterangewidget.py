363821b55e11f7c4ccc8da5d848728915f157a89
ndeebook <evers@dreamwall.be>
2024-01-12 17:08:30 +0100

    improve DateRangeFilterButton

--- dwidgets/daterangewidget.py
+++ dwidgets/daterangewidget.py
@@ -5,8 +5,8 @@ from PySide2.QtCore import Qt
 class DateRangeFilterButton(QtWidgets.QPushButton):
     dates_changed = QtCore.Signal()
 
-    def __init__(self, label, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, label, parent=None, **kwargs):
+        super().__init__(parent=parent, **kwargs)
 
         self.default_label = label
         self.setText(f' {self.default_label} (off) ')
@@ -16,7 +16,7 @@ class DateRangeFilterButton(QtWidgets.QPushButton):
         self.last_date = None
 
         # Menu
-        self.dates_menu = QtWidgets.QMenu()
+        self.dates_menu = QtWidgets.QMenu(self)
         self.cal_first = QtWidgets.QCalendarWidget()
         self.cal_last = QtWidgets.QCalendarWidget()
         cancel_button = QtWidgets.QPushButton(' Cancel ')
@@ -24,7 +24,7 @@ class DateRangeFilterButton(QtWidgets.QPushButton):
         reset_button = QtWidgets.QPushButton(' Reset ')
         reset_button.clicked.connect(self.reset_dates)
         validate_button = QtWidgets.QPushButton(' Confirm ')
-        validate_button.clicked.connect(self.set_dates)
+        validate_button.clicked.connect(self.confirm_dates)
 
         layout = QtWidgets.QVBoxLayout(self.dates_menu)
 
@@ -43,21 +43,34 @@ class DateRangeFilterButton(QtWidgets.QPushButton):
     def pop(self):
         self.dates_menu.popup(self.mapToGlobal(self.rect().bottomLeft()))
 
-    def reset_dates(self):
-        self.first_date = None
-        self.last_date = None
-        self.setText(f' {self.default_label} (off) ')
+    def set_dates(self, first_date, last_date, update_calendars=True):
+        if first_date == self.first_date and last_date == self.last_date:
+            return
+        self.first_date = first_date
+        self.last_date = last_date
+        if update_calendars:
+            if first_date:
+                self.cal_first.setSelectedDate(first_date)
+            if last_date:
+                self.cal_last.setSelectedDate(last_date)
+        if first_date is None and last_date is None:
+            label = f' {self.default_label} (off) '
+        else:
+            label = (
+                f'{self.default_label} '
+                f'({self.first_date:%d/%m/%Y} - {self.last_date:%d/%m/%Y})')
+        self.setText(label)
         self.dates_changed.emit()
         self.dates_menu.close()
 
-    def set_dates(self):
-        self.first_date = self.cal_first.selectedDate().toPython()
-        self.last_date = self.cal_last.selectedDate().toPython()
-        self.setText(
-            f'{self.default_label} '
-            f'({self.first_date:%d/%m/%Y} - {self.last_date:%d/%m/%Y})')
-        self.dates_changed.emit()
-        self.dates_menu.close()
+    def reset_dates(self):
+        self.set_dates(None, None)
+
+    def confirm_dates(self):
+        self.set_dates(
+            first_date=self.cal_first.selectedDate().toPython(),
+            last_date=self.cal_last.selectedDate().toPython(),
+            update_calendars=False)
 
     @property
     def dates(self):
