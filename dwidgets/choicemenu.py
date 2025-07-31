cd313002afe7807e97784492625b989ece03b3ca
oliver.e <evers@dreamwall.be>
2025-07-31 21:47:49 +0200

    ChoiceScrollMenu vertical arrows

--- dwidgets/choicemenu.py
+++ dwidgets/choicemenu.py
@@ -1,5 +1,5 @@
 from functools import partial
-from PySide2 import QtWidgets
+from PySide2 import QtWidgets, QtCore
 from PySide2.QtCore import Qt
 
 
@@ -27,6 +27,31 @@ class ChoiceMenu(QtWidgets.QMenu):
         self.choice = choice
 
 
+class LineEdit(QtWidgets.QLineEdit):
+    arrow_pressed = QtCore.Signal(Qt.Key)
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+    def keyPressEvent(self, event):
+        key = event.key()
+        if key in (Qt.Key_Down, Qt.Key_Up):
+            self.arrow_pressed.emit(key)
+        return super().keyPressEvent(event)
+
+
+class ListWidget(QtWidgets.QListWidget):
+    return_pressed = QtCore.Signal()
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Return:
+            self.return_pressed.emit()
+        return super().keyPressEvent(event)
+
+
 class ChoiceScrollMenu(QtWidgets.QMenu):
     """
     dialog = ChoiceScrollMenu(choices, parent=self)
@@ -41,11 +66,13 @@ class ChoiceScrollMenu(QtWidgets.QMenu):
         self.labels = labels or choices
         self.choice = None
 
-        self.search_edit = QtWidgets.QLineEdit()
+        self.search_edit = LineEdit()
+        self.search_edit.arrow_pressed.connect(self.arrow_clicked)
         self.search_edit.textChanged.connect(self.filter_choices)
 
-        self.items_list = QtWidgets.QListWidget()
+        self.items_list = ListWidget()
         self.items_list.itemClicked.connect(self.item_clicked)
+        self.items_list.return_pressed.connect(self.handle_return)
 
         self.setMinimumWidth(200)
         self.setMinimumHeight(400)
@@ -56,6 +83,10 @@ class ChoiceScrollMenu(QtWidgets.QMenu):
         layout.addWidget(self.items_list)
 
         self.filter_choices()
+
+    def showEvent(self, event):
+        self.search_edit.setFocus()
+        return super().showEvent(event)
 
     def filter_choices(self):
         search_text = self.search_edit.text().lower()
@@ -70,3 +101,12 @@ class ChoiceScrollMenu(QtWidgets.QMenu):
     def item_clicked(self, item):
         self.choice = item.data(Qt.UserRole)
         self.close()
+
+    def handle_return(self):
+        current_row = self.items_list.currentRow() or 0
+        self.item_clicked(self.items_list.item(current_row))
+
+    def arrow_clicked(self):
+        self.items_list.setFocus()
+        if not self.items_list.currentRow():
+            self.items_list.setCurrentRow(0)
